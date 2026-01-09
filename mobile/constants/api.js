75c1283beadd68dd1f22cb3f21606979b22edd64
vedchaudhari <vedc2853@gmail.com>
2026-01-09 17:39:45 +0530

    fix: updated production url

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -2,9 +2,9 @@ import Constants from "expo-constants";
 
 const localhost = Constants.expoConfig?.hostUri?.split(":")[0];
 
-export const API_URL = `http://${localhost}:3000`;
+// export const API_URL = `http://${localhost}:3000`;
 
-// export const API_URL = `https://api-bookworm.backend-portfolio-api.online`;
+export const API_URL = `https://api-bookworm.backend-portfolio-api.online`;
 
 
 // import * as Device from "expo-device";
