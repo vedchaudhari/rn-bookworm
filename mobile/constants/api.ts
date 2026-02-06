4d5572f0f31c0072781ac6aa4280226e84b838f5
vedchaudhari <vedc2853@gmail.com>
2026-02-06 10:02:25 +0530

    chore: Activate production API_URL and deactivate local emulator URL.

--- mobile/constants/api.ts
+++ mobile/constants/api.ts
@@ -1,5 +1,5 @@
 // For Android Emulator, use 10.0.2.2
-export const API_URL = `http://10.0.2.2:3000`;
+// export const API_URL = `http://10.0.2.2:3000`;
 
 //For local
 // export const API_URL = `http://localhost:3000`;
@@ -11,6 +11,6 @@ export const API_URL = `http://10.0.2.2:3000`;
 // export const API_URL = `http://192.168.29.95:3000`;
 
 //For production/EAS builds
-// export const API_URL = process.env.EXPO_PUBLIC_API_URL || `https://api-bookworm.backend-portfolio-api.online`;
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || `https://api-bookworm.backend-portfolio-api.online`;
 
 // export const API_URL = `https://148feef334d2.ngrok-free.app`;
